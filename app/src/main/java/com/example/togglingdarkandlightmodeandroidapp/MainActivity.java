96e622510aee0adde454dc24ce9f45c48b9f9aed
kevinchege47 <chegekevin001@gmail.com>
2023-02-15 12:26:46 +0300

    complete

--- app/src/main/java/com/example/togglingdarkandlightmodeandroidapp/MainActivity.java
+++ app/src/main/java/com/example/togglingdarkandlightmodeandroidapp/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.togglingdarkandlightmodeandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageview;
+    SwitchCompat switchcompat;
+    SharedPreferences sharedPreferences = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageview = findViewById(R.id.imageview);
+        switchcompat = findViewById(R.id.switchcompat);
+        sharedPreferences = getSharedPreferences("night",0);
+        Boolean booleanValue = sharedPreferences.getBoolean("nightmode",true);
+        if(booleanValue){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            switchcompat.setChecked(true);
+            imageview.setImageResource(R.drawable.night);
+        }
+        switchcompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    switchcompat.setChecked(true);
+                    imageview.setImageResource(R.drawable.night);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("nightmode",true);
+                    editor.commit();
+                }else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    switchcompat.setChecked(false);
+                    imageview.setImageResource(R.drawable.night);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("nightmode",false);
+                    editor.commit();
+                }
+            }
+        });
+
     }
 }
